68440c35f5ffd0bc6fc92eed3c49625840221ee3
Abhiram Vadali <abhiram.vadali@gmail.com>
2025-03-20 22:09:24 -0400

    Update package_setup.py
    made it a whole lot better, gui for user's convenience

--- package_setup.py
+++ package_setup.py
@@ -1,10 +1,52 @@
 # Package setup - Installs all modules required for it to run
 # This will be run when setting it up, and the end user will typically not need to run it
-# if python is updated to a later version (3.12.7 -> 3.13.2 for example), you will need to run this again
+# Includes a GUI, just in case
+# if python is upgraded to a later version (3.12.7 -> 3.13.2 for example), you will need to run this again
 
 import subprocess
+import customtkinter as ckt # Note that if customtkinter is broken, the pip command will have to be manually entered in the terminal
+import shutil
+import os
 
-subprocess.run(["powershell", "-Command", "python -m pip install pandas"], capture_output=False) 
-subprocess.run(["powershell", "-Command", "python -m pip install tabulate"], capture_output=False) 
-subprocess.run(["powershell", "-Command", "python -m pip install customtkinter"], capture_output=False) 
-subprocess.run(["powershell", "-Command", "pip install pillow"], capture_output=False) 
+def fixer():
+    subprocess.run(["powershell", "-Command", "python -m pip install pandas -q"], capture_output=False) 
+    subprocess.run(["powershell", "-Command", "python -m pip install tabulate -q"], capture_output=False) 
+    subprocess.run(["powershell", "-Command", "python -m pip install customtkinter -q"], capture_output=False) 
+    subprocess.run(["powershell", "-Command", "pip install pillow -q"], capture_output=False) 
+    lbll.configure(root, text="all required packages have been reinstalled")
+
+def reset():
+    shutil.rmtree(r"\volunteersys")
+    os.makedirs(r"\volunteersys")
+    lbll.configure(root, text="all data has been reset")
+
+def rm():
+    subprocess.run(["powershell", "-Command", "python -m pip install pandas --upgrade -q"], capture_output=False) 
+    subprocess.run(["powershell", "-Command", "python -m pip install tabulate --upgrade -q"], capture_output=False) 
+    subprocess.run(["powershell", "-Command", "python -m pip install customtkinter --upgrade -q"], capture_output=False) 
+    subprocess.run(["powershell", "-Command", "pip install pillow --upgrade -q"], capture_output=False) 
+    lbll.configure(root, text="upgraded all packages")
+
+root = ckt.CTk()
+
+root.geometry("720x480")
+root.title("volunteersys - troubleshooter")
+
+title = ckt.CTkLabel(root, text="volunteer-sys developer tools\nDon't reset unless you have to, all data will be LOST!")
+title.pack(padx=10, pady=10)
+
+setup=ckt.CTkButton(root, text="Reinstall Packages",command=fixer,fg_color="#00FF00",text_color="#000000")
+setup.pack(padx=10,pady=10)
+
+remove=ckt.CTkButton(root, text="Upgrade Packages",command=rm,fg_color="#008B8B",text_color="#000000")
+remove.pack(padx=10,pady=10)
+
+rst=ckt.CTkButton(root, text="Reset",command=reset, fg_color="#FF0000", text_color="#000000")
+rst.pack(padx=10,pady=10)
+
+lbll=ckt.CTkLabel(root, text="-----",text_color="#FF474C")
+lbll.pack(padx=10,pady=10)
+
+root.mainloop()
+
+
